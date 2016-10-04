3272eedfe1ece01849e3522bb9f168b1443c83bf
sinharsh <harsh.sinha89@gmail.com>
2016-10-04 10:51:30 +0000

    6

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var fs = require('fs');
 var server = restify.createServer();
 //server.use(academyDialog.verifyBotFramework({ appId: 'f6e40f1a-f77d-47ac-a3d3-1920ba8140bf', appSecret: 'G6E9sY593AxqAhQyBYgiTgO' }));
 //server.post('academybot/v1/messages', academyDialog.verifyBotFramework(),academyDialog.listen());
-server.listen(process.env.PORT||80 ,process.env.ip||"0.0.0.0"  , function () {
+server.listen(process.env.PORT||8080 ,process.env.ip  , function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 
